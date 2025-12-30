28fb6c7a4ce312b9b048f90883cc0bbfd6822523
Iman Zuhrah <166434695+imanzuhrah@users.noreply.github.com>
2025-12-29 21:38:23 -0300

    update 29.12.25

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.menu');
+const body = document.body;
 
 menuToggle.addEventListener('click', () => {
     menu.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+    body.classList.toggle('menu-open');
 });
 
-/* FECHAR AO CLICAR NO LINK */
+/* Fecha o menu ao clicar em qualquer link */
 document.querySelectorAll('.menu a').forEach(link => {
     link.addEventListener('click', () => {
         menu.classList.remove('active');
+        menuToggle.classList.remove('active');
+        body.classList.remove('menu-open');
     });
 });
