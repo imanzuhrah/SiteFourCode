24ca79bc9c75441da2506504da8548ac20c8b097
Iman Zuhrah <166434695+imanzuhrah@users.noreply.github.com>
2025-12-29 21:32:46 -0300

    update 29.12.25

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const toggle = document.querySelector('.menu-toggle');
+const menuToggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.menu');
 
-toggle.addEventListener('click', () => {
+menuToggle.addEventListener('click', () => {
     menu.classList.toggle('active');
+});
+
+/* FECHAR AO CLICAR NO LINK */
+document.querySelectorAll('.menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
 });
